4d833aadf0e846c6b2d427d143d4c424b0f12622
codewithblinks <udensit565@gmail.com>
2025-10-31 04:17:55 +0100

    minor update

--- src/Components/Home/ProjectList.jsx
+++ src/Components/Home/ProjectList.jsx
@@ -14,72 +14,72 @@ function ProjectList() {
                 <div className="flex-none h-[40px] w-full relative">
                     <a href="" className="h-full w-full overflow-hidden relative">
                         <section className="flex size-full max-h-full max-w-full m-0 p-0 mask-fade-x overflow-hidden place-items-center">
-                            <div className="animate-scroll">
+                            <div className="animate-scroll border border-white">
 
-                                <div className="flex items-center gap-5">
-                                    <div className="h-[22px] w-[205px] flex items-center text-lg font-semibold border border-white">
-                                        <img src={cssLogoImage} alt="" className="h-[40px] w-[205]" />
+                                <div className="flex items-center flex-nowrap gap-5">
+                                    <div className="h-[60px] w-[205px] flex items-center text-lg font-semibold border border-white">
+                                        <img src={cssLogoImage} alt="CSS logo" className="h-[60px] w-[60px]" />
                                         <h1 className="text-white">CSS</h1>
                                     </div>
-                                    <div className="h-[22px] w-[205px] flex items-center text-lg font-semibold">
-                                        <img src={htmlLogoImage} alt="" className="h-[40px] w-[205]" />
+                                    <div className="h-[60px] w-[205px] flex items-center text-lg font-semibold border border-white">
+                                        <img src={htmlLogoImage} alt="HTML logo" className="h-[60px] w-[60px]" />
                                         <h1 className="text-white">HTML</h1>
                                     </div>
-                                    <div className="h-[22px] w-[205px] flex items-center">
-                                        <img src={javascriptLogoImage} alt="" className="h-[40px] w-[205]" />
+                                    <div className="h-[60px] w-[205px] flex items-center">
+                                        <img src={javascriptLogoImage} alt="javascript logo" className="h-[60px] w-[60px]" />
                                         <h1 className="text-white">Javascript</h1>
                                     </div>
-                                    <div className="h-[22px] w-[205px] flex items-center text-lg font-semibold">
-                                        <img src={expressJSLogoImage} alt="" className="h-[40px] w-[205]" />
+                                    <div className="h-[60px] w-[205px] flex items-center text-lg font-semibold">
+                                        <img src={expressJSLogoImage} alt="Express Js logo" className="h-[60px] w-[60px]" />
                                         <h1 className="text-white">Express JS</h1>
                                     </div>
-                                    <div className="h-[22px] w-[205px] flex items-center">
-                                        <img src={nodeJSLogoImage} alt="" className="h-[40px] w-[205]" />
+                                    <div className="h-[60px] w-[205px] flex items-center">
+                                        <img src={nodeJSLogoImage} alt="Node js logo" className="h-[60px] w-[60px]" />
                                     </div>
-                                    <div className="h-[22px] w-[205px] flex items-center text-lg font-semibold">
-                                        <img src={reactLogoImage} alt="" className="h-[40px] w-[205]" />
+                                    <div className="h-[60px] w-[205px] flex items-center text-lg font-semibold">
+                                        <img src={reactLogoImage} alt="React logo" className="h-[60px] w-[60px]" />
                                         <h1 className="text-white">React JS</h1>
                                     </div>
-                                    <div className="h-[22px] w-[205px] flex items-center text-lg font-semibold">
-                                        <img src={tailwindLogoImage} alt="" className="h-[40px] w-[205]" />
+                                    <div className="h-[60px] w-[205px] flex items-center text-lg font-semibold">
+                                        <img src={tailwindLogoImage} alt="Tailwind CSS logo" className="h-[60px] w-[60px]" />
                                         <h1 className="text-white">Tailwind CSS</h1>
                                     </div>
-                                    <div className="h-[22px] w-[205px] flex items-center text-lg font-semibold">
-                                        <img src={bootstrapLogoImage} alt="" className="h-[40px] w-[205]" />
+                                    <div className="h-[60px] w-[205px] flex items-center text-lg font-semibold">
+                                        <img src={bootstrapLogoImage} alt="Bootstrap Logo" className="h-[60px] w-[60px]" />
                                         <h1 className="text-white">Bootstrap</h1>
                                     </div>
                                 </div>
 
-                                <div className="flex items-center">
-                                    <div className="h-[22px] w-[205px] flex items-center text-lg font-semibold">
-                                        <img src={cssLogoImage} alt="" className="h-[40px] w-[205]" />
+                                <div className="flex items-center flex-nowrap gap-5">
+                                    <div className="h-[60px] w-[205px] flex items-center text-lg font-semibold">
+                                        <img src={cssLogoImage} alt="" className="h-[60px] w-[60px]" />
                                         <h1 className="text-white">CSS</h1>
                                     </div>
-                                    <div className="h-[22px] w-[205px] flex items-center text-lg font-semibold">
-                                        <img src={htmlLogoImage} alt="" className="h-[40px] w-[205]" />
+                                    <div className="h-[60px] w-[205px] flex items-center text-lg font-semibold">
+                                        <img src={htmlLogoImage} alt="" className="h-[60px] w-[60px]" />
                                         <h1 className="text-white">HTML</h1>
                                     </div>
-                                    <div className="h-[22px] w-[205px] flex items-center">
-                                        <img src={javascriptLogoImage} alt="" className="h-[40px] w-[205]" />
+                                    <div className="h-[60px] w-[205px] flex items-center">
+                                        <img src={javascriptLogoImage} alt="" className="h-[60px] w-[60px]" />
                                         <h1 className="text-white">Javascript</h1>
                                     </div>
-                                    <div className="h-[22px] w-[205px] flex items-center text-lg font-semibold">
-                                        <img src={expressJSLogoImage} alt="" className="h-[40px] w-[205]" />
+                                    <div className="h-[60px] w-[205px] flex items-center text-lg font-semibold">
+                                        <img src={expressJSLogoImage} alt="" className="h-[60px] w-[60px]" />
                                         <h1 className="text-white">Express JS</h1>
                                     </div>
-                                    <div className="h-[22px] w-[205px] flex items-center">
-                                        <img src={nodeJSLogoImage} alt="" className="h-[40px] w-[205]" />
+                                    <div className="h-[60px] w-[205px] flex items-center">
+                                        <img src={nodeJSLogoImage} alt="" className="h-[60px] w-[60px]" />
                                     </div>
-                                    <div className="h-[22px] w-[205px] flex items-center text-lg font-semibold">
-                                        <img src={reactLogoImage} alt="" className="h-[40px] w-[205]" />
+                                    <div className="h-[60px] w-[205px] flex items-center text-lg font-semibold">
+                                        <img src={reactLogoImage} alt="" className="h-[60px] w-[60px]" />
                                         <h1 className="text-white">React JS</h1>
                                     </div>
-                                    <div className="h-[22px] w-[205px] flex items-center text-lg font-semibold">
-                                        <img src={tailwindLogoImage} alt="" className="h-[40px] w-[205]" />
+                                    <div className="h-[60px] w-[205px] flex items-center text-lg font-semibold">
+                                        <img src={tailwindLogoImage} alt="" className="h-[60px] w-[60px]" />
                                         <h1 className="text-white">Tailwind CSS</h1>
                                     </div>
-                                    <div className="h-[22px] w-[205px] flex items-center text-lg font-semibold">
-                                        <img src={bootstrapLogoImage} alt="" className="h-[40px] w-[205]" />
+                                    <div className="h-[60px] w-[205px] flex items-center text-lg font-semibold">
+                                        <img src={bootstrapLogoImage} alt="" className="h-[60px] w-[60px]" />
                                         <h1 className="text-white">Bootstrap</h1>
                                     </div>
                                 </div>
